3cb65d2c5562f9f1fa0d82e932f3948238f2e169
Lledely <71928776+Lledely@users.noreply.github.com>
2022-05-10 17:08:47 +0300

    Update flask_app.py

--- server/flask_app.py
+++ server/flask_app.py
@@ -1,59 +1,85 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, jsonify
 import json
 import random
+# import os
 
 from data import db_session
 from data.sessions import Sessions
 
 
 app = Flask(__name__)
+db_session.global_init('/home/Lledely/mysite/server.db')
 
 
-@app.route("/update/<int:id>", methods=["POST"])
-def update(id) -> None:
+@app.route("/update/<int:id>", methods=["POST", "GET"])
+def update(id):
     data = request.json
     db_sess = db_session.create_session()
-    session = db_sess.query(Sessions).filter(Sessions.id == id)
+    session = db_sess.query(Sessions).filter(Sessions.id == id).first()
     session.state = data["state"]
-    session.prev_move = data["prev_move"]
+    session.prev_move = data["moven"]
     session.dice = f"{random.randint(1, 6)} {random.randint(1, 6)}"
+    db_sess.commit()
+    responce = {
+        "id": session.id,
+        "state": session.state,
+        "moven": session.prev_move,
+        "dice": session.dice
+    }
+    return jsonify(responce)
 
 
-@app.route("/get/<int:id>")
+@app.route("/get/<int:id>", methods=["POST", "GET"])
 def get(id) -> json:
     db_sess = db_session.create_session()
-    session = db_sess.query(Sessions).filter(Sessions.id == id)
-    next = (["w", "b"].index(session.prev_move) + 1) % 2
+    session = db_sess.query(Sessions).filter(Sessions.id == id).first()
     response = {
         "id": id,
         "state": session.state,
-        "next": ['w', 'b'][next],
+        "moven": session.prev_move,
         "dice": session.dice
     }
-    return json.dumps(response)
+    return jsonify(response)
 
 
-@app.route("/join/<int:id>")
-def join(id):
-    return redirect(url_for(f"get/{id}"))
+@app.route("/join/<int:id>", methods=["POST", "GET"])
+def join(id) -> json:
+    db_sess = db_session.create_session()
+    session = db_sess.query(Sessions).filter(Sessions.id == id).first()
+    responce = {
+        "id": session.id,
+        "state": session.state,
+        "moven": session.prev_move,
+        "dice": session.dice
+    }
+    return json.dumps(responce)
 
 
-@app.route("/start")
-def start() -> None:
+@app.route("/start", methods=["POST", "GET"])
+def start() -> json:
     db_sess = db_session.create_session()
     session = Sessions()
-    session.state = ('w' * 15) + (' ' * 22) + ('b' * 15)
-    session.prev_move = random.choice(['w', 'b'])
+    session.state = ' '.join(['w' * 15] + ['e'] * 11 + ['b' * 15] + ['e'] * 11)
+    session.prev_move = '1'
     session.dice = f"{random.randint(1, 6)} {random.randint(1, 6)}"
     db_sess.add(session)
     db_sess.commit()
-    get()
+    responce = {
+        "id": session.id,
+        "state": session.state,
+        "moven": session.prev_move,
+        "dice": session.dice
+    }
+    return jsonify(responce)
 
 
 def main():
-    db_session.global_init("db/server.db")
+    # db_session.global_init(os.path.join(os.path.dirname(os.path.abspath(__file__)), '/home/Lledely/mysite/db/server.db'))
+    # db_session.global_init('/home/Lledely/mysite/server.db')
     app.run()
 
 
 if __name__ == '__main__':
     main()
+# db_session.global_init('/home/Lledely/mysite/server.db')
+# app.run()
